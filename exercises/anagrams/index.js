06771e79725bdd6fa734952a421ad98ac64986da
Nick Russo <nrusso1993@gmail.com>
2022-02-04 22:22:00 -0500

    anagrams worked solution

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,47 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+   let aMap = {};
+   let bMap = {};
+   
+   //'[]' creates an exclude group in regex
+   stringA = stringA.replace(/[^\w]/g,"").toLowerCase();
+   stringB = stringB.replace(/[^\w]/g,"").toLowerCase();
+   
+   function makeMap(string){
+      let map = {};
+      for(let c of string){
+         //if letter only (maybe a better way to do this?)
+         if(map[c])
+            map[c]++;
+         else
+            map[c] = 1;
+      }
+      return map;
+   }
+   
+  
+   aMap = makeMap(stringA);
+   bMap = makeMap(stringB);
+
+   let aMapLength = Object.keys(aMap).length; 
+   let bMapLength = Object.keys(bMap).length; 
+
+   if(aMapLength !== bMapLength){
+      debugger;
+      return false;
+   } else {
+      debugger;
+      for(key in aMap){
+         debugger;
+         if(aMap[key] !== bMap[key]){
+            return false;
+         }
+      }
+      return true;
+   }
+
+}
 
 module.exports = anagrams;
