9fbd09ebd9aa2b0eb07ae25181b17ef1843aa121
Nick Russo <nrusso1993@gmail.com>
2022-02-12 21:08:15 -0500

    vowels problem

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -84,7 +84,7 @@ function pyramid(n,row = 0,col = 0,numChars = 1, numSpaces = (n-1),str = ""){
 
    return pyramid(n,row,col+1,numChars,numSpaces,str);
 }
-*/
+
 
 function pyramid(n,row=0,col=0,str=""){
    let mid = Math.floor((2*n - 1) / 2);
@@ -112,7 +112,7 @@ function pyramid(n,row=0,col=0,str=""){
 
 pyramid(4);
 
-/*
+
 function pyramid(n,numChars = 1, numSpaces = (n - 1)){
    let c = '#';
    let s = ' ';
@@ -127,6 +127,31 @@ function pyramid(n,numChars = 1, numSpaces = (n - 1)){
    
 }
 */
+
+//this uses the length of the level string to determine which column you are on
+//rather than tracking this with a col variable
+function pyramid(n,row=0,level=''){
+   if(row===n)
+      return;
+
+   //log out the pyramid level
+   if(level.length === 2 * n - 1){
+      console.log(level);
+      return pyramid(n,row+1);
+   }
+
+   const mid = Math.floor((2*n - 1) / 2);
+   let add;
+   //col >= mid - row && col <= mid + row is clearer to me
+   if(mid - row <= level.length && mid + row >= level.length)
+      add = '#';
+   else
+      add = ' ';
+
+   return pyramid(n,row,level+add);
+}
+
+
 //base case will be when numChars === 2n - 1;
 //otherwise recurse through and keep adding to numchars and numspaces
 
--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -6,7 +6,24 @@
 //   vowels('Hi There!') --> 3
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
+/*
+//iterative
+function vowels(str) {
+   let vowels = {'a':true,'e':true,'i':true,'o':true,'u':true};
+   let count = 0;
 
-function vowels(str) {}
+   for(let c of str.toLowerCase()){
+      if(vowels[c])
+         count++;
+   }
+   return count;
+}
+*/
+//regex
+function vowels(str) {
+   let regex = /[aeiou]/gi;
+   let result = str.match(regex);
+   return result ? result.length : 0;
+}
 
 module.exports = vowels;
