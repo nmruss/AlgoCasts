6ba2f6b04939fc3512dcaf7a8210743451c258bd
Nick Russo <nrusso1993@gmail.com>
2023-01-04 16:10:04 -0500

    linkedlists

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -2,8 +2,99 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
 
-class LinkedList {}
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  insertFirst(data) {
+    this.head = new Node(data, this.head);
+  }
+
+  size() {
+    let counter = 0;
+    let node = this.head;
+
+    while (node) {
+      counter++;
+      node = node.next;
+    }
+
+    return counter;
+  }
+
+  getFirst() {
+    return this.head;
+  }
+
+  getLast() {
+    if (!this.head) {
+      return null;
+    }
+
+    let node = this.head;
+    while (node) {
+      if (!node.next) {
+        return node;
+      }
+      node = node.next;
+    }
+  }
+
+  clear() {
+    this.head = null;
+  }
+
+  removeFirst() {
+    if (!this.head) {
+      return;
+    }
+
+    this.head = this.head.next;
+  }
+
+  removeLast() {
+    //length -2
+    if (!this.head) {
+      return null;
+    }
+
+    //length -1
+    if (!this.head.next) {
+      this.head = null;
+      return;
+    }
+
+    //length 0 or more
+    let previous = this.head;
+    let node = this.head.next;
+
+    while (node.next) {
+      //set previous.next value to null, cutting off the last node
+      //else update the previous and node values
+      previous = node;
+      node = node.next;
+    }
+    //no next node
+    previous.next = null;
+    return;
+  }
+
+  insertLast(data) {
+    const last = this.getLast();
+    if (last) {
+      last.next = new Node(data);
+    } else {
+      this.head = new Node(data);
+    }
+  }
+}
 
 module.exports = { Node, LinkedList };
