39a464e6ac57165da4c29b9174dbbc6e943eaa6c
Nick Russo <nrusso1993@gmail.com>
2023-05-01 17:33:50 -0400

    tree bfs and dfs

--- exercises/tree/index.js
+++ exercises/tree/index.js
@@ -10,8 +10,78 @@
 // on the tree class.  Each method should accept a
 // function that gets called with each element in the tree
 
-class Node {}
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.children = [];
+  }
 
-class Tree {}
+  add(data) {
+    this.children.push(new Node(data));
+  }
+
+  remove(data) {
+    this.children = this.children.filter((item) => {
+      return item.data !== data;
+    });
+  }
+}
+
+class Tree {
+  constructor() {
+    this.root = null;
+  }
+
+  /*
+  accepts a function argument that will be called with every node in the tree
+  traverseBF(f) {
+    let arr = [];
+    const root = this.root;
+    arr.push(root);
+
+    while (arr.length > 0) {
+      const first = arr.shift();
+      let i = 0;
+      if (first[i]) f(first[i]);
+      while (i < first.length) {
+        arr.push(first.children[i]);
+        i++;
+      }
+    }
+  }
+  */
+
+  traverseBF(fn) {
+    //basic algorithm is to use a stack
+    const arr = [this.root];
+    while (arr.length) {
+      const node = arr.shift();
+
+      for (let child of node.children) {
+        arr.push(child);
+      }
+      //spread operator will push child elements like a for loop would
+      //arr.push(...node.children);
+
+      fn(node);
+    }
+  }
+
+  traverseDF(fn) {
+    //basic algorithm is to use a queue
+    const arr = [this.root];
+    while (arr.length) {
+      const node = arr.shift();
+
+      //need to iterate backwards for adding these to queue in the correct order
+      for (let i = node.children.length - 1; i >= 0; i--) {
+        arr.unshift(node.children[i]);
+      }
+      //arr.unshift(...node.children);
+
+      fn(node);
+    }
+  }
+}
 
 module.exports = { Tree, Node };
